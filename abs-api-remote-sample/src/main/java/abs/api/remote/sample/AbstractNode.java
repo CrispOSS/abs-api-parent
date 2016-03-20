661bef521dae9b889177792bae030f71a0b325d6
vladut <vladut@vladuts-macbook-air.local>
2016-03-20 23:21:20 +0100

    Distributed PA Branch on ABS-API-Remote-Sample

---
+++ abs-api-remote-sample/src/main/java/abs/api/remote/sample/AbstractNode.java
@@ -0,0 +1,45 @@
+package abs.api.remote.sample;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import abs.api.Actor;
+import abs.api.remote.ActorServer;
+
+/**
+ * @author Behrooz Nobakht
+ */
+public abstract class AbstractNode implements Node {
+
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+	protected final int id;
+	protected ActorServer server;
+	protected Actor self;
+
+	public AbstractNode(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public int getId() {
+		return id;
+	}
+
+	protected Properties getProperties() {
+		Properties p = new Properties();
+		p.setProperty("host", "localhost");
+		p.setProperty("port", String.valueOf(getPort()));
+		return p;
+	}
+
+	protected void start() throws Exception {
+		if (self == null) {
+			server = new ActorServer(getProperties());
+			self = server.context.newActor(getName(), this);
+		}
+	}
+
+}
---
+++ abs-api-remote-sample/src/main/java/abs/api/remote/sample/Node.java
@@ -0,0 +1,20 @@
+package abs.api.remote.sample;
+
+/**
+ * @author Behrooz Nobakht
+ */
+public interface Node {
+
+	int DEFAULT_PORT_START = 1888;
+
+	int getId();
+
+	default int getPort() {
+		return DEFAULT_PORT_START + getId();
+	}
+
+	default String getName() {
+		return "node-" + getId();
+	}
+
+}
